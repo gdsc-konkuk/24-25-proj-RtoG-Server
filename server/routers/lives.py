72778120e52827c576542f32af9e9f0681c63c12
Junepil Lee <44938522+junepil@users.noreply.github.com>
2025-05-14 00:21:15 +0900

    feat: 영상 스트리밍 로직 추가 (#23)
    * feat: 영상 스트리밍 로직 추가
    
    * fix: :bug: Resolve video file path bug
    
    * fix: :bug: Fix unable to call gemini api
    
    ---------
    
    Co-authored-by: jhmin99 <mjh012399@gmail.com>

--- server/routers/lives.py
+++ server/routers/lives.py
@@ -5,16 +5,22 @@ from services import LiveService, VideoProcessingService
 from schemas import LiveResponse
 from fastapi.responses import StreamingResponse
 from config import settings
+from models import Video
 
 router = APIRouter()
 
-@router.get("/{video_name}")
-async def stream_video_realtime(video_name: str):
+@router.get("/{video_id}")
+async def stream_video_realtime(video_id: str, db: Session = Depends(get_db)):
     """
     특정 영상을 실시간으로 읽어 YOLO 검증을 수행하고, 마킹된 프레임을 스트리밍합니다.
     """
-    # 설정 파일에서 경로 사용
-    video_path = f"{settings.VIDEO_STORAGE_PATH}/{video_name}"
+    # DB에서 비디오 정보 조회
+    video = db.query(Video).filter(Video.id == video_id).first()
+    if not video:
+        raise HTTPException(status_code=404, detail="Video not found")
+    
+    # 비디오 파일 경로
+    video_path = f"{settings.VIDEO_STORAGE_PATH}/{video.id}.mp4"
     print(f"Attempting to stream video from: {video_path}")
     return StreamingResponse(
         VideoProcessingService.frame_generator_with_yolo(video_path), 
@@ -28,23 +34,21 @@ async def lives_endpoint(db: Session = Depends(get_db)):
     
     특징:
     - Live 탭 진입 시 프론트엔드에서 호출
-    - 각 CCTV의 ID, 이름, 설치 위치(address), WebSocket ID를 포함
+    - 각 CCTV의 ID, 이름, 설치 위치(address)를 포함
     
     반환 예시:
     ```json
     {
       "cctvs": [
         {
-          "id": "cctv_001",
+          "id": "video_001",
           "name": "강릉시청 앞 CCTV-1",
-          "address": "강원도 강릉시",
-          "socket_id": "ws_001"
+          "address": "강원도 강릉시"
         },
         {
-          "id": "cctv_002",
+          "id": "video_002",
           "name": "강릉시청 앞 CCTV-2",
-          "address": "강원도 강릉시",
-          "socket_id": "ws_002"
+          "address": "강원도 강릉시"
         }
       ]
     }
